cefbea2ea3f5304d133d2bd68e7d2fe8477b6f59
JunSeok Kim <skaksdl1238@gmail.com>
2022-10-05 19:23:28 +0900

    Initial commit

---
+++ src/main/java/com/example/SsulooLook/SsulooLookApplication.java
@@ -0,0 +1,15 @@
+package com.example.SsulooLook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsulooLookApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SsulooLookApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/example/SsulooLook/SsulooLookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SsulooLook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsulooLookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
